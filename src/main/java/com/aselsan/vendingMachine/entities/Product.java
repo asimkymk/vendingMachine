2f4e04cbe911fedce8212bd49d2eff86a77bb7f8
Asım Kaymak <kaymakasm@gmail.com>
2023-09-18 15:33:12 +0300

    Initial commit

---
+++ src/main/java/com/aselsan/vendingMachine/entities/Product.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.aselsan.vendingMachine.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+/**
+ *
+ * @author asimk
+ */
+@Entity
+@Table(name="product")
+@Data
+public class Product {
+    @Id
+    Long id;
+    
+    String productName;
+    int productPrice;
+    int productAmount;
+}
