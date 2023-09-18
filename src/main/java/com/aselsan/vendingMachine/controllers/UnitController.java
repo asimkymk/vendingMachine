72c827eb1f70b16ca89e970de9a0dfd21b104e35
Asım Kaymak <kaymakasm@gmail.com>
2023-09-18 16:34:20 +0300

    feat: create unit controller

---
+++ src/main/java/com/aselsan/vendingMachine/controllers/UnitController.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.aselsan.vendingMachine.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author asimk
+ */
+@RestController
+@RequestMapping("/units")
+public class UnitController {
+    
+}
